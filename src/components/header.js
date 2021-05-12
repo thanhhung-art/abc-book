615bf4de63eb7a6fef95cfa2d8b63a0bf3b55a6f
thanhhung-pc <tranphong3457@gmail.com>
2021-05-12 17:40:04 +0700

    init project

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__top">
+                <div className="header__top__logo">
+                    <img src="../images/best-books1.jpg" alt="logo"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
